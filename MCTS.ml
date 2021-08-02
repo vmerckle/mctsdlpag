db1f86a22721db59a5bfdaaa567a53038b1933d7
Victor Mercklé <11957227+vmerckle@users.noreply.github.com>
2021-08-02 09:13:10 +0200

    init

---
+++ MCTS.ml
@@ -0,0 +1,133 @@
+open Printf
+open MCTSutils
+
+(*type stats = { n : int;
+      nwin : int;
+      ntoprove : int;
+}
+
+type oparbre = And | Or | Not
+type feuilletype = Formu of SS.t * iformula | Direct of bool
+type state = Proven of bool | Ni
+type arbre = Noeud of (state * arbre list * oparbre * stats) | Feuille of feuilletype *)
+
+exception Found of state * int
+exception Unexpec 
+
+let isProven t = match t with
+  | Feuille(Direct b) | Noeud(Proven b, _,_,_) -> Proven b
+  | _ -> Ni (*check children's state now?*)
+ 
+let resolved t = match isProven t with
+Proven _ -> true
+  | _ -> false
+
+let calcUCB n stat = let nf, rf, nif = float_of_int n, float_of_int stat.nwin, float_of_int stat.n in rf/.nif +. 0.1 *. sqrt(log(nf)/.nif) 
+
+(* select one node with highest UCT or never explored in a list, returns it and its position in the list *)
+let selectone n l = let _= if false then eprintf "\nDeciding between %d ..\n%s" (List.length l) (Print.list' "" "\n-->" "\n\n"  Print.arbreK l)in match l with [] -> assert false
+  | l -> let rec aux maxUCT maxNode maxNum num l = match l with
+    | [] -> maxNode, maxNum
+    | h::t ->  if resolved h then aux maxUCT maxNode maxNum (num+1) t
+    else
+        begin match h with
+      | Feuille(Direct _) -> aux maxUCT maxNode maxNum (num+1) t
+      | Feuille(Formu(_, _)) as x -> x, num
+      | Noeud(_, _ , _ , stat) as newp when stat.n = 0 -> newp, num
+      | Noeud(_, _ , _, stat) as newp -> let score = calcUCB n stat in
+      let _= if stat.n > (-1) then if false then eprintf "select : %s(max=%f) -> %f\n" (Print.arbre newp) maxUCT score in 
+        if score > maxUCT then aux score newp num (num+1) t  else aux maxUCT maxNode maxNum (num+1) t
+      end
+  in let choice, num = aux (-0.9) (Feuille(Direct true)) (-1) 0 l in let _= if false then eprintf "Chosen the %d = %s\n\n" (num+1) (Print.arbre choice) in choice,num
+
+let notyetExplored stat = stat.n = 0
+(* goes through a tree t by selection to find a node not yet played out once = a feuille or
+ a noeud with zero played 
+ returns : the updated selected node, and a path to it [1,2,1] = take first child, take second, take first and you are on the node*)
+let selectexpand n t =  
+  match t with
+  | Feuille(Direct _) -> let _= eprintf "found a feuille directly " in raise Unexpec (* shouldn't happen *)
+  | _ -> begin 
+    let rec aux path tree = let _= if false then eprintf "selectexpand : %s\n" (Print.arbre tree) in 
+    match tree with
+    | Feuille(Direct _) ->let _= eprintf "ffound a feuille directly " in  raise Unexpec(* shouldn't happen *)
+    | Feuille(Formu(v, f)) -> (splitIF v f, path)
+    | Noeud(_, l, _, stat) as newp -> if (notyetExplored stat) then (newp, path) else let choice,num = selectone n l in
+      aux (num::path) choice
+   in let newp, path = aux [] t in
+   (newp, List.rev path)
+  end
+
+let chooseone t =
+  let i = Random.int (List.length t) in
+  List.nth t i
+
+let rec playout t =
+  match t with
+  | Feuille(Direct b) -> b
+  | Feuille(Formu(v, f)) -> playout (splitIF v f)
+  | Noeud(_, l, op, s) ->  match l with 
+    | [] -> false
+    | [h] -> let x = playout h (* necessary & opti *) in (match op with Not -> not x | _-> x)
+    | h::t ->  match op with
+        | And -> playout h  && playout (Noeud(Ni, t, op, s))
+        | Or -> let newp = chooseone (h::t) in playout newp
+        | Not -> not (playout h )
+
+
+let replace l i newp = let rec aux liste count acc = match liste with
+  [] -> List.rev acc
+        | _::t when count=i -> (List.rev (newp::acc)) @ t
+        | h::t -> aux t (count+1) (h::acc)
+     in aux l 0 []
+
+let nprovecalc ntoprove childB op = match op with
+    And -> (match childB with Ni -> ntoprove, Ni
+      | Proven true -> ntoprove - 1, (if (ntoprove -1) = 0 then Proven true else Ni)
+      | Proven false -> 0, Proven false )
+    | Or -> (match childB with Ni -> ntoprove, Ni
+      | Proven true -> 0, Proven true
+      | Proven false -> ntoprove - 1, (if (ntoprove -1) = 0 then Proven false else Ni))
+    | Not -> match childB with Ni -> 1, Ni
+      | Proven childB -> 0, Proven (not childB)
+(* return t with all nodes in path updated with res=bool, b=state of the last number of path and newp its node *)
+let rec update tree newp path res = match path with [] -> newp 
+    | h::t-> match tree with
+        | Noeud(_, l, op, stat) -> let newt = update (List.nth l h) newp t res in
+        let newl = replace l h newt in
+        let b = isProven newt in
+        let newnprove, newb = nprovecalc stat.ntoprove b op in
+        let nstat = {n=stat.n+1 ; nwin=stat.nwin + (if res then 1 else 0); ntoprove=newnprove} in
+        Noeud(newb, newl, op, nstat)
+        | _ -> raise Unexpec
+
+(* take a node, its proven state and last res from playout -> return the node with updated stats *)
+(* here we don't replace a proven Noeud by a feuille so that we don't forget some part of the tree, as we might need this subtree as part of the proof *)
+let updatenode x res b = match x with
+  Noeud(_, l, op, s) -> Noeud(b, l, op, {n=s.n+1; nwin=s.nwin + (if res then 1 else 0); ntoprove=s.ntoprove})
+  |y -> y
+
+let solve f n =
+  let t = ref (splitIF (SS.empty) f )in
+  try
+    for i=0 to n do
+      let _= if false then eprintf "arbre now = %s\n" (Print.arbre !t) in 
+      let _= if false then eprintf "%d ----------------- New MCTS LOOP\n" i in 
+      let proof = isProven !t in match proof with
+      | Proven b -> raise (Found(Proven b, i))
+      | Ni -> begin
+        let newp, path = selectexpand i !t in
+        let _= if false then eprintf "Path now = %s\n" (String.concat "," (List.map string_of_int path)) in 
+        let b = isProven newp in
+        let res = match b with
+        | Proven b -> b
+        | Ni -> playout newp
+        in
+        let newps = updatenode newp res b in
+        (*let _= eprintf "newp = %s, res = %b\n" (Print.arbre newp) res in 
+        let _= eprintf "newps = %s, res = %b\n" (Print.arbre newps) res in *)
+        t := update !t newps path res 
+      end
+    done ; (Ni,n)
+  with Found (x,i) -> x,i
+  
---
+++ MCTSutils.ml
@@ -0,0 +1,215 @@
+open Printf
+
+module CSet = Set.Make (struct type t = Circuit.T.callable let compare = compare end)
+module SS = Set.Make (struct type t = string let compare = compare end)
+exception Not_implem of string
+
+type iformula = CallF of bool * string | Base of bool | ListF of (Ast.T.foperator * iformula list) | Modal of (bool * iprogram * iformula) (*| Not of iformula*)
+
+and iprogram  = Assign of (string * iformula) | Test of iformula | ListP of (Ast.T.poperator * iprogram list) | LE of (int * iprogram)
+
+
+(*** Usual matching ***)
+(*
+  | Formu(v, f) -> (match f with
+    | CallF (b, s) -> 
+    | Base b ->  
+    | ListF(Ast.T.Conj, l) -> 
+    | ListF(Ast.T.Disj, l) -> 
+    | Modal(b, p, f) -> (match p with
+      | Assign(s, fa) -> let vs = SS.add s v and vns = SS.remove s v in 
+      | Test fp -> 
+      | ListP(Ast.T.Seq, l) -> (match l with 
+        | [] -> 
+        | [h] -> 
+        | h::t -> 
+      )
+      | ListP(Ast.T.U, l) -> 
+      | LE(n, p) -> ( match n with
+        |0 -> 
+        |1 -> 
+        |n ->
+      )
+    )
+  )
+*)
+
+type stats = { n : int;
+      nwin : int;
+      ntoprove : int;
+}
+
+type oparbre = And | Or | Not
+type feuilletype = Formu of SS.t * iformula | Direct of bool
+type state = Proven of bool | Ni
+type arbre = Noeud of (state * arbre list * oparbre * stats) | Feuille of feuilletype
+
+
+module Print =
+struct
+
+let list' ld id rd pr l = sprintf "%s%s%s" ld (String.concat id (List.map pr l)) rd
+let list pr = list' "[" "; " "]" pr
+
+  let rec iformula = function
+    | CallF(b, s) -> sprintf "%s%s" (if b then "" else "-") s
+    | Base true -> sprintf "T"
+    | Base false -> sprintf "-T"
+    | ListF(Ast.T.Conj, l) -> sprintf  "(%s)" (Print.list' "" "^" "" iformula l) 
+    | ListF(Ast.T.Disj, l) -> sprintf  "(%s)" (Print.list' "" " v " "" iformula l) 
+    | Modal(true, p, f) -> sprintf "<%s>%s" (program p) (iformula f)
+    | Modal(false, p, f) -> sprintf "[%s]%s" (program p) (iformula f)
+  and program = function
+    | Assign(s, f) -> (match f with
+      | Base true -> sprintf "+%s" s
+      | Base false -> sprintf "-%s" s
+      | _ -> sprintf "%s <- %s" s (iformula f) )
+    | Test f -> sprintf "(%s)?" (iformula f)
+    | ListP(Ast.T.Seq, l) -> sprintf "%s" (Print.list' "" "; " "" program l)
+    | ListP(Ast.T.U, l) -> sprintf "%s" (Print.list' "" " U " "" program l)
+    | LE(n, p) -> sprintf "(%s)^<=%d" (program p) n
+
+  let set' ld id rd l = match l with
+    | l when SS.cardinal l = 0-> ""
+    | l -> sprintf "%s%s%s " ld (String.concat id (SS.elements l)) rd
+  let rec arbre = function
+    (*| Noeud(_, l, op, _) -> sprintf "%s(%s)"  ((function And -> "And" | Or -> "Or " | Not -> "Not") op)  (Print.list' "" ", " "" arbre l)*)
+    | Noeud(st, l, op, _) -> sprintf "%s%s(%s)"  ((function And -> "And" | Or -> "Or" | Not -> "Not") op)  ((function Ni -> "" | Proven true -> "(T)" | Proven false -> "(F)")st) (Print.list' "" ", " "" arbre l)
+    | Feuille(Formu(v, f)) -> sprintf "%s|=%s" (set' "{" "," "}" v) (iformula f)
+   | Feuille(Direct(b)) -> ((function true -> "T" | false -> "F") b)
+
+  let rec arbreK = function
+    (*| Noeud(_, l, op, _) -> sprintf "%s(%s)"  ((function And -> "And" | Or -> "Or " | Not -> "Not") op)  (Print.list' "" ", " "" arbre l)*)
+    | Noeud(st, l, op, se) -> sprintf "[%d]%s%s(%s)"  se.n ((function And -> "And" | Or -> "Or" | Not -> "Not") op)  ((function Ni -> "" | Proven true -> "(T)" | Proven false -> "(F)")st) (Print.list' "" ", " "" arbreK l)
+    | Feuille(Formu(v, f)) -> sprintf "%s|=%s" (set' "{" "," "}" v) (iformula f)
+   | Feuille(Direct(b)) -> ((function true -> "T" | false -> "F") b)
+
+   let arbreq = function
+    (*| Noeud(_, l, op, _) -> sprintf "%s(%s)"  ((function And -> "And" | Or -> "Or " | Not -> "Not") op)  (Print.list' "" ", " "" arbre l)*)
+    | Noeud(st, l, op, _) -> sprintf "%s%s(%d)"  ((function And -> "And" | Or -> "Or" | Not -> "Not") op)  ((function Ni -> "" | Proven true -> "(T)" | Proven false -> "(F)")st) (List.length l)
+    | Feuille(Formu(v, _)) -> sprintf "%s|=" (set' "{" "," "}" v) 
+    | Feuille(Direct(b)) -> ((function true -> "T" | false -> "F") b)
+end
+
+let rec countP p = match p with
+  | Formula.Assign(a, psi) -> CSet.add a (countF psi)
+  | Formula.ListP(_, l) -> List.fold_right CSet.union (List.map countP l) CSet.empty
+  | Formula.Test psi -> countF psi
+  | Formula.Kleene p -> countP p
+  | Formula.Converse _ -> raise (Not_implem "Converse")
+and countF f = match f with
+  | Formula.CallF(_, a) ->CSet.singleton a
+  | Formula.Base _ -> CSet.empty
+  | Formula.ListF(_, l) -> List.fold_right CSet.union(List.map countF l) CSet.empty
+  | Formula.Modal(_, p, f) -> CSet.union (countP p) (countF f)
+
+let acountF f = CSet.cardinal (countF f)
+let acountP p = CSet.cardinal (countP p)
+
+let rec pow a = function
+  | 0 -> 1
+  | 1 -> a
+  | n -> 
+    let b = pow a (n / 2) in
+    b * b * (if n mod 2 = 0 then 1 else a)
+
+(* avoid overflow *)
+let twopow n = match n with
+n  when n <= 32 -> pow 2 n
+  | _ -> max_int
+
+
+let rec formToIForm (f:Formula.formula) : iformula = match f with
+  | Formula.CallF(b, a) -> let _= if false then eprintf "---> %s %b\n" (Circuit.Print.callable a) b in 
+      let s = Circuit.Print.callable a in CallF(b, s)
+  | Formula.Base b -> Base b
+  | Formula.ListF(op, l) -> ListF(op, List.map formToIForm l)
+  | Formula.Modal(b, p, f) -> let fi = formToIForm f in
+    match p with
+    | Formula.ListP(Ast.T.Seq, xt) ->  (* this isn't working properly obviously TODO *)
+        ( match xt with
+        | [] -> formToIForm f (* skip.. = <;> *)
+        | [h] -> Modal(b, progToIProg h, fi)
+        | h::t -> Modal(b, progToIProg h, formToIForm (Formula.Modal(b, ListP(Ast.T.Seq, t), f)))
+      )
+    | _ -> let _=eprintf"still\n" in Modal(b, progToIProg p, fi)
+
+and progToIProg (p:Formula.program) : iprogram = match p with
+  | Formula.Assign(a, psi) -> Assign(Circuit.Print.callable a, formToIForm psi)
+  | Formula.ListP(op, l) -> ListP(op, List.map progToIProg l) (*should convert Seq close to above, but not really necessary as we won't use this function directly *)
+  | Formula.Test psi -> Test (formToIForm psi)
+  | Formula.Kleene p -> LE(twopow (acountP p) , progToIProg p)
+  | Formula.Converse _ -> raise (Not_implem "Converse")
+
+let statInit l = {n=0; nwin=0; ntoprove=l}
+let toFormu (v:SS.t) (f:iformula) : arbre = Feuille(Formu(v, f))
+let ptoFormu v f b p = Feuille(Formu(v, Modal(b, p, f)))
+let bDiff = function true -> Or | false -> And (* simple diff between box and diamonds *)
+
+(* valuation,formula -> tree *)
+let splitIF (v:SS.t) (f:iformula) : arbre = match f with
+  | CallF (b, s) -> Feuille (Direct (let x =(SS.mem s v) in if b then x else not x))
+  | Base b ->  Feuille (Direct b)
+  | ListF(Ast.T.Conj, l) -> Noeud(Ni, List.map (toFormu v) l, And, statInit (List.length l))
+  | ListF(Ast.T.Disj, l) -> Noeud(Ni, List.map (toFormu v) l, Or, statInit (List.length l))
+  | Modal(b, p, f) -> match p with
+    | Assign(s, fa) -> let vs = SS.add s v and vns = SS.remove s v in
+        Noeud(Ni, [Noeud(Ni, [Feuille(Formu(v, fa)); Feuille(Formu(vs, f))], And, statInit 2);
+               Noeud(Ni, [Noeud(Ni, [Feuille(Formu(v, fa))], Not, statInit 1); Feuille(Formu(vns, f))], And, statInit 2) ] , Or, statInit 2)
+    | Test fp -> Noeud(Ni, [Feuille(Formu(v, fp)); Feuille(Formu(v, f))], And, statInit 2)
+    | ListP(Ast.T.Seq, l) -> (match l with 
+        | [] -> toFormu v f (* <;>f -> f *)
+        | [h] -> Feuille(Formu(v, Modal(b, h, f))) (* <p;> -> <p> *)
+        | h::t -> Feuille(Formu(v, Modal(b, h, Modal(b, ListP(Ast.T.Seq, t), f))))(*<p;q>-> <p><q>*)
+    )
+    | ListP(Ast.T.U, l) -> Noeud(Ni, List.map (ptoFormu v f b) l, bDiff b,statInit (List.length l))
+    | LE(n, p) -> match n with
+        |0 -> toFormu v f (* <p^<=0>f -> f *)
+        |1 -> Noeud(Ni, [toFormu v (Modal(b, p, f));toFormu v f], bDiff b, statInit 2) (*opti*)
+        |n -> Noeud(Ni, [toFormu v (Modal(b, p, Modal(b, LE(n-1, p), f)));toFormu v f], bDiff b, statInit 2)
+
+let kek n st = let rec aux n s = match n with
+0 -> s
+        | n -> aux (n-1) (s^st)
+in aux n ""
+
+let rec playthrough t b n = 
+  let _= if b then eprintf "%s%s {\n" (kek n "---- ") (Print.arbre t) in 
+  let x =
+  match t with
+  | Feuille(Direct b) -> b
+  | Feuille(Formu(v, f)) -> let x = playthrough (splitIF v f) b (n+1) in x
+  | Noeud(_, l, op, s) ->  match l with 
+    | [] -> false
+    | [h] -> let x = playthrough h b (n+1) (* necessary & opti *) in (match op with Not -> not x | _-> x)
+    | h::t ->  match op with
+        | And -> playthrough h b (n+1)  && playthrough (Noeud(Ni, t, op, s)) b (n+1)
+        | Or -> playthrough h b (n+1) || playthrough (Noeud(Ni, t, op, s)) b (n+1)
+        | Not -> not (playthrough h b (n+1))
+   in
+  let _= if b then eprintf "%s} %b \n" (kek n "     ") x  in x
+
+let rec playthroughc t = 
+  match t with
+  | Feuille(Direct b) -> b,1
+  | Feuille(Formu(v, f)) -> let x,y = playthroughc (splitIF v f) in x,y+1
+  | Noeud(_, l, op, s) ->  match l with 
+    | [] -> false,0
+    | [h] -> let x,y = playthroughc h (* necessary & opti *) in (match op with Not -> not x | _-> x), y+1
+    | h::t ->  match op with
+        | And -> let bx, nx = playthroughc h in if not bx then bx, nx+1 else let by, ny = playthroughc (Noeud(Ni, t, op, s)) in bx && by , nx+ny+1
+        | Or -> let bx, nx = playthroughc h in if bx then bx, nx+1 else let by, ny = playthroughc (Noeud(Ni, t, op, s)) in bx || by , nx+ny+1
+        | Not -> not (fst(playthroughc h) ),0
+
+
+
+let rec playthroughcount t c = 
+  match t with
+  | Feuille(Direct _) -> c+1
+  | Feuille(Formu(v, f)) -> playthroughcount (splitIF v f) (1+c)
+  | Noeud(_, l, op, s) ->  match l with 
+    | [] -> c
+    | [h] -> playthroughcount h (c+1)
+    | h::t ->  match op with
+        | And|Or -> playthroughcount h (playthroughcount (Noeud(Ni, t, op, s)) (c+1))
+        | Not -> playthroughcount h (c+1)
